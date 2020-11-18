e779134af271d37296a31365d330a079b1ddfe10
lrani <lrani@cisco.com>
2020-11-18 13:58:56 +0530

    Initial Draft

---
+++ 1.0/src/main/java/com/demo/App.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public void CustomClassCall(String s) {
+        System.out.println("Packaged class called" + s);
+    }
+   
+}
